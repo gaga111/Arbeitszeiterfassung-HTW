63116a3b0287a18f07d57d66f0e1a881e14eb259
Galina Kanunnikova <galinakanunnikova@Galinas-MacBook.local>
2019-06-02 22:51:27 +0200

    version 0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NaviComponent } from './navi/navi.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatInputModule, MatFormFieldModule,MatFormFieldControl } from '@angular/material';
 import { TimetableComponent } from './timetable/timetable.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NaviComponent,
-    TimetableComponent
+    TimetableComponent,
   ],
   imports: [
+    MatSelectModule,
+    FormsModule,
+    MatCheckboxModule,
     BrowserModule,
     BrowserAnimationsModule,
     LayoutModule,
